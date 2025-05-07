a645e56322a8c105753b079ed77240d1bc12ac4c
celeri <celestin.bouchet@gmail.com>
2025-05-07 10:21:41 +0200

    init

---
+++ src/test/java/io/github/MSPR4_2025/orders_service/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.MSPR4_2025.orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
